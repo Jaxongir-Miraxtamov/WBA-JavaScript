a0812f3fa87f8f52ddd05f0766541dd9b0110f3c
Jaxongir-Miraxtamov <jaxongirmiraxtamov@gmail.com>
2023-03-01 14:27:07 +0500

    functions

---
+++ lesson5/index.js
@@ -0,0 +1,51 @@
+// exam 1
+
+for (let i = 0; i < 3; i++) {
+  console.log(`number ${i}!`);
+}
+
+// exam2
+let age = 18;
+function checkAge(age) {
+  if (age > 18) {
+    return true;
+  } else {
+    age < 18;
+    return false;
+  }
+  return confirm("Did parents allow you");
+}
+
+// exam3
+
+function checkAge(age) {
+  if (age > 18) {
+    return true;
+  } else {
+    age < 18;
+    return false;
+  }
+  return confirm("Did parents allow you");
+}
+
+// exam4
+function min(a = 0, b = 0) {
+  let res = a / b;
+
+  return res;
+}
+console.log(parseInt(min(2, 5)));
+console.log(parseInt(min(3, -1)));
+console.log(min(1, 1));
+
+// name(a, b);
+
+// exam5
+
+// function mmm(x, n) {
+//   return x ** n;
+//   console.log(pow(3, 2));
+//   console.log(pow(3, 3));
+//   console.log(pow(1, 100));
+// }
+// mmm(x, n);
