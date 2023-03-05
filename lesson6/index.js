1d0eae66af562d6058c8d6bb4cd98f16b4a33702
Jaxongir-Miraxtamov <jaxongirmiraxtamov@gmail.com>
2023-03-05 22:14:25 +0500

    Objects

--- lesson6/index.js
+++ lesson6/index.js
@@ -8,3 +8,22 @@ user.name = "Pete";
 delete user.name;
 
 console.log(user);
+
+// exam3'
+
+// let salaries = {
+//   john: 100,
+//   Ann: 160,
+//   Pete: 130,
+// };
+// salaries == 0;
+
+// console.log(salaries);
+
+// salaries = 0 > 200;
+// if (0 > 200) john: 100, 0 > 200;
+// let salaries = {
+//   Ann: 160,
+//   Pete: 130,
+// };
+// console.log(salaries);
